58b179b7a8afc9ef5f8afc0f37dc0ab674d5c94a
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-03-31 19:48:24 -0500

    script modificado

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
+
+const btnCheck = document.querySelector('.check');
+const btnAgain = document.querySelector('.again');
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+btnCheck.addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+btnAgain.addEventListener('click', () => {
+  score = 20;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '25rem';
+  document.querySelector('.number').textContent = '?';
+});
