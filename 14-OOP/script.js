a1c0067efbc163b42c3cd25d44419123e984bcc0
Renzo Osorio <renzo.osorio1909@gmail.com>
2024-02-13 21:30:25 -0500

    agregar seccion 14

---
+++ 14-OOP/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName
+  this.birthYear = birthYear
+}
+
+const jonas = new Person('Jonas', 1991)
+
+const matilda = new Person('Matilda', 2017)
+const jack = new Person('Jack', 1975)
