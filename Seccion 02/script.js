956a00f4a006529fbfbb9893716a9ee34ba98aa3
Renzo Osorio Valdez <renzo.osorio1909@gmail.com>
2026-03-10 21:36:50 -0500

    feat: añadir Sección 02 - variables, tipos de datos y operadores

---
+++ Seccion 02/script.js
@@ -0,0 +1,91 @@
+/*let js = 'amazing'
+console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas')
+console.log(23)
+
+let firstName = 'Matilda'
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_matilda = 'JM'
+let $function = 27
+
+let person = 'jonas'
+let PI = 3.1415
+
+let myFirstJob = 'Coder'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'programmer'
+let job2 = 'teacher'
+
+console.log(myFirstJob)
+
+console.log("-------------------------------")
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log(typeof javascriptIsFun)
+console.log(typeof 23)
+console.log(typeof 'Jonas')
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
+
+console.log("-------------------------------")
+
+let age = 30
+age = 31
+
+const birthYear = 1991
+// birthYear = 1990
+// const job
+
+var job = 'programmer'
+job = 'teacher'
+
+lastName = 'Schmedtmann'
+console.log(lastName)
+
+console.log("-------------------------------")*/
+
+// Math Operators
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+const firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+console.log(firstName + ' ' + lastName)
+
+// Assignment Operators
+let x = 10 + 5
+x += 10
+x *= 4
+x++
+x--
+console.log(x)
+
+// Comparison Operators
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+console.log(now - 1991 > now - 2018)
